85ef7bebf449e15d80d5b74f3832c3bbcdbf1fe1
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-11-27 09:03:28 +0100

    basic setup

---
+++ symfplay/src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    namespace App\Controller;
+
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class TestController extends AbstractController
+    {
+        /**
+         * @Route("/test")
+         */
+        public function test():Response {
+            return $this->render('home.html.twig');
+        }
+    }
