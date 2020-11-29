94e41010c3a8d90ca49e9952d520452988edb2a7
Oladayo Fagbemi <oofagbemi@outlook.com>
2020-11-29 04:08:36 +0100

    added project entitiy and some dummy data to DB

---
+++ symfplay/src/Controller/ProjectController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Project;
+use App\Repository\ProjectRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProjectController extends AbstractController
+{
+    /**
+     * @Route("/project", name="project")
+     */
+    public function index(): Response
+    {
+        return $this->render('project/index.html.twig', [
+            'controller_name' => 'ProjectController',
+        ]);
+    }
+
+    /**
+     * @Route("/create",name="project-create")
+     */
+    public function create(ProjectRepository $projectRepository)
+    {
+        $project=$projectRepository->find(1);
+        if (!$project) {
+            $em = $this->getDoctrine()->getManager();
+            $projects = [
+                ['title'=>'Project 1','description'=>'This is my first project'],
+                ['title'=>'Project 2','description'=>'build a new Warehouse'],
+                ['title'=>'Project 3','description'=>'Rebuild the basement'],
+                ['title'=>'Project 4','description'=>'Prepare thesis presentation'],
+            ];
+            foreach ($projects as $project) {
+
+                $proj = new Project();
+                $proj->setTitle($project['title']);
+                $proj->setDescription($project['description']);
+                $em->persist($proj);
+            }
+            $em->flush();
+            return $this->render('home.html.twig',['msg'=>'New Projects created and written to DB']);
+
+        }
+        return $this->render('home.html.twig',['msg'=>'Some projects already exist in the DB']);
+
+
+    }
+
+}
--- symfplay/src/Controller/TestController.php
+++ symfplay/src/Controller/TestController.php
@@ -5,15 +5,20 @@
 
 
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
 
     class TestController extends AbstractController
     {
         /**
-         * @Route("/test")
+         * @Route("/")
+         * @param Request $request
+         * @return Response
          */
-        public function test():Response {
-            return $this->render('home.html.twig');
+        public function test(Request $request):Response {
+            $ipAdd = $request->getClientIp();
+            dump($ipAdd);
+            return $this->render('home.html.twig',compact('ipAdd'));
         }
     }
